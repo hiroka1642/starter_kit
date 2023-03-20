8458108e23d036838f611302e6747da33092793e
hiroka1642 <uver7770422@gmail.com>
2023-03-20 16:00:54 +0900

    tsx 用に webpack.config.js を編集する

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,13 @@ module.exports = {
   // モジュールバンドルを行う起点となるファイルの指定
   // 指定できる値としては、ファイル名の文字列や、それを並べた配列やオブジェクト
   // 下記はオブジェクトとして指定した例
-  entry: {
-    bundle: "./src/app.ts",
-  },
+  // 例
+  // entry: {
+  //   bundle: "./src/app.ts",
+  // },
+
+  // https://qiita.com/toshi-toma/items/080b89000ed7f0242bee#2-webpack%E3%81%AE%E8%A8%AD%E5%AE%9Awebpackconfigjs
+  entry: "./src/index.tsx",
   output: {
     // モジュールバンドルを行った結果を出力する場所やファイル名の指定
     // "__dirname"はこのファイルが存在するディレクトリを表すnode.jsで定義済みの定数
@@ -16,7 +20,8 @@ module.exports = {
   // 例えば「import Foo from './foo'」という記述に対して"foo.ts"という名前のファイルをモジュールとして探す
   // デフォルトは['.js', '.json']
   resolve: {
-    extensions: [".ts", ".js"],
+    //https://qiita.com/toshi-toma/items/080b89000ed7f0242bee#2-webpack%E3%81%AE%E8%A8%AD%E5%AE%9Awebpackconfigjs
+    extensions: [".ts", ".tsx", ".js"],
   },
   devServer: {
     // webpack-dev-serverの公開フォルダ
@@ -28,8 +33,9 @@ module.exports = {
   module: {
     rules: [
       {
-        // 拡張子が.tsで終わるファイルに対して、TypeScriptコンパイラを適用する
-        test: /\.ts$/,
+        // 拡張子が.tsxで終わるファイルに対して、TypeScriptコンパイラを適用する
+        // https://qiita.com/toshi-toma/items/080b89000ed7f0242bee#2-webpack%E3%81%AE%E8%A8%AD%E5%AE%9Awebpackconfigjs
+        test: /\.tsx?$/,
         loader: "ts-loader",
       },
 
